6ba59a8bdecd3686fa03a55639d2467ac75f8165
JuliusScript <satriastar77@gmail.com>
2026-05-30 22:04:50 +0700

    .

--- supabase/migrations/20250523120000_add_store_operating_hours.sql
+++
@@ -1,9 +0,0 @@
--- Jam operasional per toko (kasir hanya bisa akses di rentang ini)
--- NULL pada open_time/close_time = tidak ada batasan (24 jam)
-
-ALTER TABLE public.stores
-  ADD COLUMN IF NOT EXISTS open_time time,
-  ADD COLUMN IF NOT EXISTS close_time time;
-
-COMMENT ON COLUMN public.stores.open_time IS 'Jam buka toko (WIB). NULL = tidak dibatasi.';
-COMMENT ON COLUMN public.stores.close_time IS 'Jam tutup toko (WIB). NULL = tidak dibatasi.';
